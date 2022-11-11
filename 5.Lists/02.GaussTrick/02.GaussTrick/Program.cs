0bc86ceba4bdef40124af57dc3b4580686ece50c
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2022-11-11 11:09:14 +0200

    Add List exercise

---
+++ 5.Lists/02.GaussTrick/02.GaussTrick/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.GaussTrick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split() //On default splits by space
+                .Select(int.Parse) //we should parse the elements of the list in the correct format
+                .ToList(); 
+            SumNumbersInList(numbers);
+
+        }
+
+        static void SumNumbersInList(List<int> numbers)
+        {
+
+            int originalLength = numbers.Count;
+            int numOfLoops = originalLength / 2;
+
+            for (int i = 0; i < numOfLoops / 2; i++)
+            {
+                numbers[i] += numbers[numbers.Count - 1];
+                numbers.RemoveAt(numbers.Count - 1);
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+
+        }
+
+    }
+}
---
+++ 5.Lists/03.MergingLists/03.MergingLists/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MergingLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            List<int> list2 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            CreateResultList(list1, list2);
+
+        }
+
+        static void CreateResultList(List<int> list1, List<int> list2)
+        {                             
+            List<int> resultList = new List<int>();
+            int biggerList = Math.Max(list1.Count, list2.Count);
+
+            for (int i = 0; i < biggerList; i++)
+            {
+                if (list1.Count > i)
+                {
+                resultList.Add(list1[i]);
+                }
+                if (list2.Count > i)
+                {
+                resultList.Add(list2[i]);
+                }                            
+            }
+
+            Console.WriteLine(string.Join(" ", resultList));
+        }
+    }
+}
