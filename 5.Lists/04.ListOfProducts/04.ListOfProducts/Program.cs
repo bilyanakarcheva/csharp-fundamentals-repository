c7a9d1f98b403bdd4bbe4b1ed523944a90c6b46e
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2022-11-11 12:09:27 +0200

    Add List exercises

---
+++ 5.Lists/04.ListOfProducts/04.ListOfProducts/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.ListOfProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintNumberedListOfProducts(n);
+        }
+
+        static void PrintNumberedListOfProducts(int n)
+        {
+            List<string> listOfProducts = new List<string>();
+            
+
+            for (int i = 0; i < n; i++)
+            {
+                string currentProduct = Console.ReadLine();
+                listOfProducts.Add(currentProduct);
+            }
+
+            listOfProducts.Sort();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"{i}.{listOfProducts[i-1]}");
+            }
+
+        }
+    }
+}
