9afd3f41d4e53e43595282e6c03557699cf80e3f
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2022-11-13 23:35:44 +0200

    Add List excercises

---
+++ 5.Lists/05.RemoveNegativesAndReverse/05.RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.RemoveNegativesAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            RemoveNegativeAndPrintInReverse(numbers);
+        }
+
+        static void RemoveNegativeAndPrintInReverse(List<int> numbers)
+        {
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] < 0)
+                {
+                    numbers.RemoveAt(i);
+                    i -= 1;
+                }
+            }
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            numbers.Reverse();
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
