6e754957d95ba078a26a1e645712028baa1ac345
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2022-11-14 23:26:49 +0200

    Add List exercises

---
+++ 5.Lists/06.ListManipulationBasics/06.ListManipulationBasics/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.ListManipulationBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            ListManipulation(numbers);
+        }
+
+        static void ListManipulation(List<int> numbers)
+        {
+
+            string command = Console.ReadLine(); // Add 3
+
+            while (command != "end")
+            {
+                string[] elements = command.Split();
+                
+                switch (elements[0])
+                {
+                    case "Add":
+                        numbers.Add(int.Parse(elements[1].ToString())); // Check element of string
+                        break;
+                    case "Remove":
+                        numbers.Remove(int.Parse(elements[1].ToString()));
+                        break;
+                    case "RemoveAt":
+                        numbers.RemoveAt(int.Parse(elements[1].ToString()));
+                        break;
+                    case "Insert":
+                        int number = int.Parse(elements[1]);
+                        int index = int.Parse(elements[2]);
+                        numbers.Insert(index, number); // !!IMPORTANT!!
+                        break;
+                    default:
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
