54994cea5ea996577ed49901c20e55bb471154df
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2022-11-22 13:24:13 +0200

    Ädd Count Real Numbers exercise

---
+++ 7.Associative Arrays - Dictionaries/01.CountRealNumbers/01.CountRealNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            SortedDictionary<int, int> counts = new SortedDictionary<int, int>();
+
+            // Always need to check if the Key already exists first!!!
+
+            foreach (var number in numbers)
+            {
+                if (counts.ContainsKey(number)) 
+                {
+                    counts[number]++;
+                }
+                else
+                {
+                    counts.Add(number, 1);
+                }
+            }
+            foreach (var number in counts)
+            {
+
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+
+            }
+        }
+    }
+}
