99efbe9415711b931611af2b672ea75844ea156e
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2022-11-23 12:58:19 +0200

    New version of solution

---
+++ 5.Lists/04.ListOfProductsV2/04.ListOfProducts/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int numOfLines = int.Parse(Console.ReadLine());
+
+List<string> lines = new List<string>();
+
+for (int i = 0; i < numOfLines; i++)
+{
+    string line = Console.ReadLine();
+
+        //(i + 1) + "." + line;
+    
+    lines.Add(line);
+}
+
+lines.Sort();
+
+for (int i = 0; i < lines.Count; i++)
+{
+    string currentLine = lines[i];
+    lines[i] = (i + 1) + "." + currentLine;
+}
+
+Console.WriteLine(string.Join(Environment.NewLine, lines));
