8a68f25957f3fb0064c145293c736b113e693bc7
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2022-11-23 09:46:31 +0200

    Add Dictionaries exercise

---
+++ 7.Associative Arrays - Dictionaries/02.OddOccurrences/02.OddOccurrences/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lineOfWords = Console.ReadLine().Split().ToArray();
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            foreach (var word in lineOfWords)
+            {
+                string wordInLowerCase = word.ToLower();
+
+                if (counts.ContainsKey(wordInLowerCase))
+                {
+                    counts[wordInLowerCase]++;
+                }
+                else
+                {
+                    counts.Add(wordInLowerCase, 1);
+                }
+            }
+
+            foreach (var word in counts)
+            {
+                if (word.Value % 2 != 0)
+                {
+                    Console.Write(word.Key + " ");
+                }
+            }
+        }
+    }
+}
