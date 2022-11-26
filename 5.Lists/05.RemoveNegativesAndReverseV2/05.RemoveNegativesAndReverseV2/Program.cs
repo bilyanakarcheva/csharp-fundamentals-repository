b3c7fbff8b20e00e290ce82aa3328c84fd638558
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2022-11-26 15:10:09 +0200

    Add Remove Negative and Reverse V2

---
+++ 5.Lists/05.RemoveNegativesAndReverseV2/05.RemoveNegativesAndReverseV2/Program.cs
@@ -0,0 +1,63 @@
+﻿List<int> numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    if (numbers[i] < 0)
+    {
+        numbers.RemoveAt(i);
+        i--;
+    }
+
+}
+
+if (numbers.Count == 0)// Check
+{
+    Console.WriteLine("empty");
+}
+else
+{
+    numbers.Reverse();
+    Console.WriteLine(String.Join(" ", numbers));
+}
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+
+//namespace _05.RemoveNegativesAndReverse
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            List<int> numbers = Console.ReadLine()
+//                .Split()
+//                .Select(int.Parse)
+//                .ToList();
+
+//            for (int i = 0; i < numbers.Count; i++)
+//            {
+//                if (numbers[i] < 0)
+//                {
+//                    numbers.RemoveAt(i);
+//                    i--;
+//                }
+
+//            }
+
+//            if (numbers.Count == 0)// Check
+//            {
+//                Console.WriteLine("empty");
+//            }
+//            else
+//            {
+//                numbers.Reverse();
+//                Console.WriteLine(String.Join(" ", numbers));
+//            }
+//        }
+
+//    }
+//}
