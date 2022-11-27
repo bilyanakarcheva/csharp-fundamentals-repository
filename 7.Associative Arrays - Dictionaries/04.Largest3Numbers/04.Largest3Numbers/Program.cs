787c15b41701e5dee4fa2dae43d4c2bf55eab2de
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2022-11-27 21:09:13 +0200

    Add Largest 3 numbers

---
+++ 7.Associative Arrays - Dictionaries/04.Largest3Numbers/04.Largest3Numbers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] sorted = numbers.OrderByDescending(n => n).ToArray();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(string.Join(' ', numbers));
+
+            }
+
+
+            //List<int> numbers = Console.ReadLine()
+            //    .Split()
+            //    .Select(int.Parse)
+            //    .ToList();
+
+            //if (numbers.Count <= 3)
+            //{
+            //    Console.WriteLine(string.Join(' ', numbers));
+            //    return;
+            //}
+
+            //List<int> largest3Nums = new List<int>();
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    int currentMax = numbers.Max();
+            //    largest3Nums.Add(currentMax);
+            //    numbers.Remove(currentMax);
+            //}
+
+            //Console.WriteLine(string.Join(' ', largest3Nums));
+
+        }
+
+    }
+}
