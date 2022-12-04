951a8c83f537a1c1ab69fc666b9a9ca0d8e84dff
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2022-12-04 15:00:02 +0200

    Add Train exercise

---
+++ 5.Lists/Additional Exercises/01.Train/01.Train/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _01.Train
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOfWagons = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int maxCapacity = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] tokens = input
+                    .Split()
+                    .ToArray();
+
+                if (tokens[0] == "Add")
+                {
+                    listOfWagons.Add(int.Parse(tokens[1]));
+                }
+                else
+                {
+                    for (int i = 0; i < listOfWagons.Count; i++)
+                    {
+                        int passengers = int.Parse(tokens[0]);
+                        if (listOfWagons[i] >= maxCapacity)
+                        {
+                            continue;
+                        }
+                        else if ((listOfWagons[i] + passengers) <= maxCapacity)
+                        {
+                            listOfWagons[i] += passengers;
+                            break;
+                        }
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(String.Join(' ', listOfWagons));
+        }
+    }
+}
