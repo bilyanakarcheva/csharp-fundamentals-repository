7da4326108101c1ee4f91b4a6e876da9dfcf9994
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2022-12-08 17:30:22 +0200

    Add House Party V2

---
+++ 5.Lists/Additional Exercises/03.HousePartyV2/03.HousePartyV2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _03.HousePartyV2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfCommands = int.Parse(Console.ReadLine());
+
+            List<string> peopleGoing = new List<string>();
+
+            for (int i = 0; i < numOfCommands; i++)
+            {
+                string[] command = Console.ReadLine()
+                    .Split()
+                    .ToArray();
+
+                string name = command[0];
+
+                if (!command.Contains("not"))
+                {
+                    if (peopleGoing.Contains(name))
+                    {
+                        Console.WriteLine($"{name} is already in the list!");
+                    }
+                    else
+                    {
+                        peopleGoing.Add(name);
+                    }
+                }
+                else if (command.Contains("not"))
+                {
+                    if (peopleGoing.Contains(name))
+                    {
+                        peopleGoing.Remove(name);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{name} is not in the list!");
+                    }
+                }
+            }
+
+            Console.WriteLine(String.Join(Environment.NewLine, peopleGoing));
+        }
+    }
+}
