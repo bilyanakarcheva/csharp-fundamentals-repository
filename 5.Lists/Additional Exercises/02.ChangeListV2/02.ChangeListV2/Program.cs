4b5e9d29807cf5ad860685e9e9ac518b7edbde7f
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2022-12-08 12:47:39 +0200

    Add Change List V2

---
+++ 5.Lists/Additional Exercises/02.ChangeListV2/02.ChangeListV2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _02.ChangeListV2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<int> numbers = Console.ReadLine() //system.collection.generic
+                .Split()
+                .Select(int.Parse) //system.linq
+                .ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] command = input.Split();
+
+                if (command[0] == "Delete")
+                {     
+
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        if (numbers[i] == int.Parse(command[1]))
+                        {
+                            numbers.Remove(numbers[i]);
+                        }
+                    }
+                }
+                else if (command[0] == "Insert")
+                {
+                    int index = int.Parse(command[2]);
+                    int item = int.Parse(command[1]);
+                    numbers.Insert(index, item);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(String.Join(' ', numbers));
+        }
+    }
+}
