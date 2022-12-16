b4fbaafae4b5a2b25caff172a1355d32765c0796
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2022-12-16 20:43:16 +0200

    Add Reverese Array of Strings

---
+++ 3.Arrays/04.ReverseArrayОfStrings/04.ReverseArrayОfStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04.ReverseArrayОfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine(); // -1 hi ho w
+
+            string[] array = input.Split(); 
+
+            for (int i = 0; i < array.Length / 2; i++) // dividing by 2 because we exchange one element for another therefore we have to do half as much iterations of the loop
+            {
+                string oldElement = array[i];
+                array[i] = array[array.Length - i - 1];
+                array[array.Length - i - 1] = oldElement;
+            }
+
+            Console.WriteLine(String.Join(" ", array));
+        }
+    }
+}
