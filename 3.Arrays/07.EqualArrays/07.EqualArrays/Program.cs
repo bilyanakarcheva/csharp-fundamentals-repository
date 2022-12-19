5152d692d826d1cb9816ba11e582375d02c20240
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2022-12-19 23:48:20 +0200

    Add Equal Arrays

---
+++ 3.Arrays/07.EqualArrays/07.EqualArrays/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.EqualArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] array2 = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int sum = 0;
+            bool isIdentical = true;
+            int index = 0;
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                sum += array1[i];
+
+                if (array1[i] != array2[i])
+                {
+                    index = i;
+                    isIdentical = false;
+                    break;
+                }
+               
+            }
+
+            if (isIdentical)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+            else
+            {
+                Console.WriteLine($"Arrays are not identical. Found difference at {index} index");
+            }
+        }
+    }
+}
