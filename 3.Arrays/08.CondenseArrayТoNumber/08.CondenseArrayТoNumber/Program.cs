5920e12c1e420a3b5ca3052a0319206ba696de99
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2022-12-20 11:22:46 +0200

    Add Condense Array to Number

---
+++ 3.Arrays/08.CondenseArrayТoNumber/08.CondenseArrayТoNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.CondenseArrayТoNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+
+            while (nums.Length > 1)
+            {
+                int[] array = new int[nums.Length - 1];
+
+                for (int i = 0; i < array.Length; i++)
+                {
+
+                    array[i] = nums[i] + nums[i + 1];
+
+                }
+                nums = array;
+
+            }
+
+            Console.WriteLine(String.Join(' ', nums));
+        }
+    }
+}
