d6f329f5df248d1d4c9f1f908210098b116130d9
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2022-12-23 20:08:45 +0200

    Ädd Train

---
+++ 3.Arrays/Additional exercises/01.Train/01.Train/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] peopleOnEachWagon = new int[n];
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                peopleOnEachWagon[i] = int.Parse(Console.ReadLine());
+                sum += peopleOnEachWagon[i];
+            }
+
+            Console.Write(String.Join(' ', peopleOnEachWagon));
+            Console.WriteLine();
+            Console.WriteLine(sum);
+
+        }
+    }
+}
