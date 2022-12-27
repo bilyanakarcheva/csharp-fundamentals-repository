5339f61e626c90ffb1a3290dcb0c0a4783976f65
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2022-12-27 19:27:12 +0200

    Add Common Elements

---
+++ 3.Arrays/Additional exercises/02.CommonElements/02.CommonElements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace _02.CommonElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = Console.ReadLine().Split().ToArray();
+            string[] array2 = Console.ReadLine().Split().ToArray();
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                for (int j = 0; j <array1.Length; j++)
+                {
+                    if (array1[j] == array2[i])
+                    {
+                        Console.Write(array2[i] + " ");
+                    }
+                }
+            }
+        }
+    }
+}
