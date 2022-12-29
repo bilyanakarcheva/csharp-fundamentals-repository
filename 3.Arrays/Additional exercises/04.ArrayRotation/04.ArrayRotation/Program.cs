d16f29e2665d58c92c0177089599464af855de1b
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2022-12-29 22:19:18 +0200

    Add Array Rotation

---
+++ 3.Arrays/Additional exercises/04.ArrayRotation/04.ArrayRotation/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.ArrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Трябва да обхождаме масива и да го презаписваме във временен масив - темп.
+
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int rotations = int.Parse(Console.ReadLine());
+
+            //
+
+            for (int i = 0; i < rotations; i++)
+            {
+                int firstElement = array[0];
+
+                int[] temp = new int[array.Length];
+
+                for (int j = 1; j < array.Length; j++)
+                {
+                    temp[j - 1] = array[j];
+                }
+
+                temp[temp.Length - 1] = firstElement;
+                array = temp;
+            }
+
+            Console.WriteLine(String.Join(' ', array));
+        }
+    }
+}
