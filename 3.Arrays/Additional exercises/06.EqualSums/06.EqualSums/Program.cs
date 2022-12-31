c5a1555362fdae7b8863887df0940eb967102dcd
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2022-12-31 16:22:01 +0200

    Add Equal Sums

---
+++ 3.Arrays/Additional exercises/06.EqualSums/06.EqualSums/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.EqualSums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray(); // 1 2 3 3
+            //bool isThereEqualSum = false;
+            //int index = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                int leftSum = 0;
+                int rightSum = 0;
+
+                for (int j = 0; j < i; j++)
+                {
+                    leftSum += array[j];
+                }
+                for (int k = i + 1; k < array.Length; k++)
+                {
+                    rightSum += array[k];
+                }
+
+                if (leftSum == rightSum)
+                {
+                    //index = i;
+                    //isThereEqualSum = true;
+                    //break;
+                    Console.WriteLine(i);
+                    return; // the program finishes when it's met with this function
+                }
+            }
+            //if (isThereEqualSum)
+            //{
+            //    Console.WriteLine(index);
+            //}
+            //else
+            //{
+                Console.WriteLine("no");
+            //}
+        }
+    }
+}
