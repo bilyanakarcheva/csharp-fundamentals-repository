563e8be019ad8ccf8b336775392a95e5422a34d3
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2023-01-05 16:00:18 +0200

    Add Kamino Factory

---
+++ 3.Arrays/Additional exercises/09.KaminoFactory/09.KaminoFactory/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _09.KaminoFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte length = byte.Parse(Console.ReadLine());
+            string[] command = ;
+
+            while (command != "Clone them!")
+            {
+
+            }
+
+        }
+    }
+}
