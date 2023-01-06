d21b5e76c21eee82acdd7be740284c06c9a86898
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2023-01-06 12:17:17 +0200

    Add Top Integers

---
+++ 3.Arrays/Additional exercises/505.TopIntegers/505.TopIntegers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _505.TopIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray(); // 14 24 3 19 15 17
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                bool isTopInteger = true;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] <= array[j])
+                    {
+                        isTopInteger = false;
+                        break;
+                    }
+                }
+
+                if (isTopInteger)
+                {
+                    Console.Write(array[i] + " ");
+                }
+            }
+
+
+        }
+    }
+}
