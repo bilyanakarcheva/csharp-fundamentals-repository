b2c8bad5aae0312347363986c3f025893de483af
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2023-01-08 14:18:13 +0200

    Add Sign of Integer Numbers

---
+++ 4.Methods/01.SignOfIntegerNumbers/01.SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01.SignOfIntegerNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintSignOfInteger(n);
+
+        }
+
+        static void PrintSignOfInteger(int n)
+        {
+            if (n < 0)
+            {
+                Console.WriteLine($"The number {n} is negative.");
+            }
+            else if (n > 0)
+            {
+                Console.WriteLine($"The number {n} is positive.");
+            }
+            else
+            {
+                Console.WriteLine($"The number 0 is zero.");
+            }
+        }
+    }
+}
