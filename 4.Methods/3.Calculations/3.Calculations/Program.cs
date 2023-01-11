2868246873ab141b6985561d761e58d9b985258f
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2023-01-11 11:52:17 +0200

    Add Calculations

---
+++ 4.Methods/3.Calculations/3.Calculations/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _3.Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mathAction = Console.ReadLine();
+
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            switch (mathAction)
+            {
+                case "add":
+                    AdditioOfNumbers(num1, num2);
+                    break;
+                case "multiply":
+                    MiltiplicationOfNumbers(num1, num2);
+                    break;
+                case "subtract":
+                    SubstractionOfNumbers(num1, num2);
+                    break;
+                case "divide":
+                    DivisionOfNumbers(num1, num2);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        static void AdditioOfNumbers(int num1, int num2)
+        {
+            Console.WriteLine(num1 + num2);
+        }
+
+        static void MiltiplicationOfNumbers(int num1, int num2)
+        {
+            Console.WriteLine(num1 * num2);
+        }
+
+        static void SubstractionOfNumbers(int num1, int num2)
+        {
+            Console.WriteLine(num1 - num2);
+        }
+
+        static void DivisionOfNumbers(int num1, int num2)
+        {
+            Console.WriteLine(num1 / num2);
+        }
+    }
+}
