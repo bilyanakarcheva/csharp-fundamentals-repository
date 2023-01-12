072c1a3186f3afd16ed90d41563e2d07ff5de554
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2023-01-12 10:10:48 +0200

    Add Printing Triangle

---
+++ 4.Methods/04.PrintingTriangle/04.PrintingTriangle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _04.PrintingTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintTriangle(n);
+        }
+
+        static void PrintNumbersFrom1(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void PrintTriangle(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                PrintNumbersFrom1(i);
+            }
+            for (int i = n - 1; i > 0; i--)
+            {
+                PrintNumbersFrom1(i);
+            }
+        }
+    }
+}
