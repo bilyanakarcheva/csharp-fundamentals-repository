dfec45f05e8d53370a44001f9e921dc2d80abd20
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2023-01-14 13:22:35 +0200

    Add Calculate Rectangle Area

---
+++ 4.Methods/6.CalculateRectangleArea/6.CalculateRectangleArea/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _6.CalculateRectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+          
+            Console.WriteLine(CalculateRectangleArea(width, height));
+        }
+
+        static int CalculateRectangleArea(int width, int height)
+        {
+            return width * height;
+        }
+
+
+    }
+}
