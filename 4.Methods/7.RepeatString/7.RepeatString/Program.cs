94b67d4f18683766cf3b74f4df10b5161e8c7ebe
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2023-01-15 13:01:23 +0200

    Add Repeat String

---
+++ 4.Methods/7.RepeatString/7.RepeatString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _7.RepeatString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(RepeatStringNTimes(str1, n));
+        }
+
+        static string RepeatStringNTimes(string str1, int n)
+        {
+            string newString = String.Empty;
+            
+            for (int i = 0; i < n; i++)
+            {
+                newString += str1;
+            }
+
+            return newString;
+        }
+    }
+}
