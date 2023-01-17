06e29e14265559d9716ad9b0843719b8c6803b50
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2023-01-17 20:19:10 +0200

    Add Math Power

---
+++ 4.Methods/8.MathPower/8.MathPower/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Reflection;
+
+namespace _8.MathPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine()); // number could be floating-point
+            int power = int.Parse(Console.ReadLine()); // power could only be integer
+
+            Console.WriteLine(PowOfANumber(num, power));
+        }
+
+        static double PowOfANumber(double num, int power)
+        {
+            return Math.Pow(num, power);
+        }
+    }
+}
