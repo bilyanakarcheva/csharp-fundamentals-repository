00b109f44c71a2e0f0ea1e61890b2d59c4361202
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2023-01-26 21:19:43 +0200

    Add Characters In Range

---
+++ 4.Methods/Additional exercises/03.CharactersInRange/03.CharactersInRange/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _03.CharactersInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char charStart = char.Parse(Console.ReadLine());
+            char charEnd = char.Parse(Console.ReadLine());
+            CharactersInBetween2Chars(charStart, charEnd);
+        }
+
+        static void CharactersInBetween2Chars(char charStart, char charEnd)
+        {
+            if (charStart > charEnd)
+            {
+                for (int i = charEnd + 1; i < charStart; i++)
+                {
+
+                    Console.Write((char)i + " ");
+                }
+            }
+            else
+            {
+                for (int i = charStart + 1; i < charEnd; i++)
+                {
+
+                    Console.Write((char)i + " ");
+                }
+            }
+        }
+    }
+}
