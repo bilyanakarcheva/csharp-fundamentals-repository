0d3de41993a296f8910d60ee3a8d502490e7bbd1
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2023-02-11 14:46:51 +0200

    Add Courses

---
+++ 7.Associative Arrays - Dictionaries/Additional exercises/06.Courses/06.Courses/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+            while (input != "end")
+            {
+                string[] tokens = input.Split(" : ");
+                string courseName = tokens[0];
+                string studentName = tokens[1];
+
+                if (!courses.ContainsKey(courseName))
+                {
+                    courses.Add(courseName, new List<string>() { studentName });
+
+                }
+                else
+                {
+                    courses[courseName].Add(studentName);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            var orderedCourses = courses.OrderByDescending(n => n.Value.Count);
+
+
+            foreach (var pair in orderedCourses)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value.Count}");
+
+                var orderedStudents = pair.Value.OrderBy(n => n).ToList();
+
+                foreach (var item in orderedStudents)
+                {
+                    Console.WriteLine($"-- {item}");
+                }
+            }
+        }
+    }
+}
