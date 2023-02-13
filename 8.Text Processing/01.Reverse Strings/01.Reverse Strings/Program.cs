fa641df9aa2901ff9d06543aa7ea54213d326db0
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2023-02-13 22:49:15 +0200

    Add Reverse Strings

---
+++ 8.Text Processing/01.Reverse Strings/01.Reverse Strings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine(); // helLo 
+
+            while (word != "end")
+            {
+                string reversed = string.Empty;
+
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    reversed += word[i];
+                }
+                Console.WriteLine($"{word} = {reversed}");
+
+                word = Console.ReadLine();
+            }
+
+
+        }
+    }
+}
