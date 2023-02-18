cfde51885cc13814d69ecef3d9be61f34ada67af
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2023-02-18 11:14:23 +0200

    Add Repeat Strings

---
+++ 8.Text Processing/02.Repeat Strings/02.Repeat Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02.Repeat_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayOfStrings = Console.ReadLine().Split(); // work
+
+            for (int i = 0; i < arrayOfStrings.Length; i++)
+            {
+
+                for (int j = 0; j < arrayOfStrings[i].Length; j++)
+                {
+                    Console.Write(arrayOfStrings[i]);
+                }
+            }
+        }
+    }
+}
