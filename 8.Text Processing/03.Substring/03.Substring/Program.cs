98afaaf81dc27a77728b14b54a4e4d339e03add1
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2023-02-19 15:07:03 +0200

    Add Substring

---
+++ 8.Text Processing/03.Substring/03.Substring/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Transactions;
+
+namespace _03.Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string forRemoval = Console.ReadLine();
+            string longerString = Console.ReadLine();
+
+            int currentIndex = longerString.IndexOf(forRemoval);
+
+            while (currentIndex >= 0)
+            {              
+                longerString = longerString.Remove(currentIndex, forRemoval.Length);
+                currentIndex = longerString.IndexOf(forRemoval);
+            }
+
+            Console.WriteLine(longerString);
+        }
+    }
+}
