f1e89630ffa58d22af94aaf3cd8ffc254f34ddfe
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2023-02-22 22:31:08 +0200

    Add Text Filter

---
+++ 8.Text Processing/04.Text Filter/04.Text Filter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04.Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] banList = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            foreach (var banWord in banList)
+            {
+
+                string replacementWord = new string('*', banWord.Length);
+                text = text.Replace(banWord, replacementWord); // With Replace the original string has to be overriten by the new string.
+
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
