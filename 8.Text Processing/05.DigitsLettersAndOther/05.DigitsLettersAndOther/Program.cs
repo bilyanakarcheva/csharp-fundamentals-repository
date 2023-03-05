7dc0b7d8997ff1b5c846b52444635c10689144f4
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2023-03-05 12:48:25 +0200

    Add Digital Letters and Other

---
+++ 8.Text Processing/05.DigitsLettersAndOther/05.DigitsLettersAndOther/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace _05.DigitsLettersAndOther
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder numbers = new StringBuilder();
+            StringBuilder letters = new StringBuilder();
+            StringBuilder symbols = new StringBuilder();
+
+
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+                if (char.IsDigit(currentChar))
+                {
+                    numbers.Append(input[i]);
+                }
+                else if (char.IsLetter(currentChar))
+                {
+                    letters.Append(input[i]);
+                }
+                else
+                {
+                    symbols.Append(input[i]);
+                }
+            }
+
+            Console.WriteLine(numbers);
+            Console.WriteLine(letters);
+            Console.WriteLine(symbols);
+
+        }
+    }
+}
