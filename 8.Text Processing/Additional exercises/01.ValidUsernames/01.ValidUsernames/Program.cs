88ecfe2f2320370a85f1b7b6216ed69427e7ff93
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2023-03-07 21:40:05 +0200

    Add Valid Usernames

---
+++ 8.Text Processing/Additional exercises/01.ValidUsernames/01.ValidUsernames/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.ValidUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] users = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            List<string> validUsernames = new List<string>();
+
+            foreach (var item in users)
+            {
+                bool isValid = true;
+                if (item.Length >= 3 && item.Length <= 16)
+                {
+                    for (int i = 0; i < item.Length; i++)
+                    {
+                        if (!(item[i] == '_' || item[i] == '-' || char.IsLetterOrDigit(item[i])))
+                        {
+                            isValid = false;
+                        }
+                    }
+
+                    if (isValid)
+                    {
+                        validUsernames.Add(item);
+                    }
+                }
+            }
+
+            Console.WriteLine(String.Join(Environment.NewLine, validUsernames));
+        }
+    }
+}
