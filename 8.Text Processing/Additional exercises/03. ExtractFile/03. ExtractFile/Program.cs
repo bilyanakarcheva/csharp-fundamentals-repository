2f4485239c4b90569cc37bac778f26b81c2ff3aa
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2023-03-09 22:50:37 +0200

    Add Extract file

---
+++ 8.Text Processing/Additional exercises/03. ExtractFile/03. ExtractFile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _03._ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] path = Console.ReadLine().Split('\\', StringSplitOptions.RemoveEmptyEntries);
+
+            var file = path[path.Length - 1].Split('.');
+
+            Console.WriteLine($"File name: {file[0]}");
+            Console.WriteLine($"File extension: {file[1]}");
+
+        }
+    }
+}
