6411739f7aa2abadbf2bae81febd2545455f0995
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2023-03-13 20:28:16 +0200

    Add Caesar Cipher

---
+++ 8.Text Processing/Additional exercises/04.CaesarCipher/04.CaesarCipher/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _04.CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            StringBuilder encryptedText = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                encryptedText.Append((char)(input[i] + 3));
+            }
+
+            Console.WriteLine(encryptedText);
+
+        }
+    }
+}
