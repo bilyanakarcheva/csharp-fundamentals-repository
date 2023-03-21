fbf41d57beb06b5ebf56d1237e5669fca0522558
Bilyana Karcheva <bilyana_karcheva@mail.bg>
2023-03-21 14:06:23 +0200

    Add Multiply Big Number

---
+++ 8.Text Processing/Additional exercises/05.MultiplyBigNumber/05.MultiplyBigNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace _05.MultiplyBigNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string bigNumber = Console.ReadLine();
+            int singleDigit = int.Parse(Console.ReadLine());
+            int currentNum = 0;
+
+            StringBuilder result = new StringBuilder();
+
+
+            for (int i = 0; i < bigNumber.Length; i++)
+            {
+                currentNum = bigNumber[i] * singleDigit;
+
+                if (currentNum >  9)
+                {
+                    currentNum /= 10;
+                    currentNum %= 10;
+                }
+            }
+        }
+    }
+}
